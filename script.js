1f5ffca78e40ac25c6bdee3186a8aa6fddc0fc8c
Marco Antonio <marcostoff71@gmail.com>
2022-06-19 00:34:50 -0500

    Fix bugs

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const spanContainer = document.querySelector(".container-pregunta").getElementsByTagName("span").item(0);
+const preguntas=['¿Me pasas otras fotitos?','¿Me pasas videitos?','¿Me pasas audios?']
 const btnSi =document.getElementById("btnSi");
 const btnNo =document.getElementById("btnNo");
+const txtPalabra= document.getElementById("txtPalabra");
+let index=-1;
 function agregaAnimacion(){
+
+    index++;
+    if(index==preguntas.length)index=0;
+    
+    
+    txtPalabra.innerHTML=preguntas[index];
     spanContainer.classList.remove("animate-pregunta");
     void spanContainer.offsetWidth;
     spanContainer.classList.add("animate-pregunta");
+    // spanContainer.style.animationTimingFunction=`steps(${preguntas[index].length})`;
 }
 window.addEventListener('DOMContentLoaded',()=>{
 })
@@ -56,9 +66,10 @@ btnSi.addEventListener('click',()=>{
 
     modal.classList.add('modal--show');
     setTimeout(()=>{
-        modal.classList.remove('modal--show');
-        
-    },2000);
+        while(modal.classList.contains('modal--show')){
+            modal.classList.remove('modal--show');
+        }
+    },4000);
     setTimeout(()=>{
 
         mandarMensaje();
